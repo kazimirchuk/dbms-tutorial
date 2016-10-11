6b067180226647d5c1d42d99daf11323647d9bf3
Alik Rakhmonov <alik.rahmonov@gmail.com>
2016-10-11 14:46:23 +0300

    add university db

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@ Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.network "forwarded_port", guest: 80, host: 8080
   config.vm.network "private_network", ip: "192.168.33.16"
-  config.vm.synced_folder "./", "/home/vagrant/", create: true, :nfs => true
+  config.vm.synced_folder "./", "/home/vagrant/", create: true
   config.vm.provider "virtualbox" do |vb|
     vb.memory = "512"
   end
---
+++ dumps/students/s_08/schema.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS students CASCADE;
+DROP TABLE IF EXISTS teachers CASCADE;
+DROP TABLE IF EXISTS services CASCADE;
+DROP TABLE IF EXISTS teachers_services CASCADE;
+DROP TABLE IF EXISTS students_teachers_setvices CASCADE;
+
+CREATE TABLE students(
+    id               SERIAL PRIMARY KEY,
+    first_name       VARCHAR(20) NOT NULL,
+    last_name        VARCHAR(20) NOT NULL,
+    born_year        SMALLINT NOT NULL CONSTRAINT positive_age CHECK (born_year > 0)
+);
+
+CREATE TABLE teachers(
+   id               SERIAL PRIMARY KEY,
+   first_name       VARCHAR(20) NOT NULL,
+   last_name        VARCHAR(20) NOT NULL,
+   born_year        SMALLINT NOT NULL CONSTRAINT positive_age CHECK (born_year > 0),
+   experience	    SMALLINT NOT NULL CONSTRAINT positive_experience CHECK (experience > 0)
+);
+
+CREATE TABLE subjects(
+   id               SERIAL PRIMARY KEY,
+   name             VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE teachers_subjects(
+   id               SERIAL PRIMARY KEY,
+   id_teacher       INT REFERENCES teachers (id),
+   id_subject       INT REFERENCES subjects (id)
+);
+
+CREATE TABLE services(
+    id               SERIAL PRIMARY KEY,
+    name             VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE teachers_services(
+   id               SERIAL PRIMARY KEY,
+   id_teacher       INT REFERENCES teachers (id),
+   id_service       INT REFERENCES services (id),
+   price            SMALLINT NOT NULL CONSTRAINT positive_price CHECK (price > 0)
+);
+
+CREATE TABLE students_teachers_setvices(
+   id                    SERIAL PRIMARY KEY,
+   id_student            INT REFERENCES countries (id),
+   id_teacher_service    VARCHAR(50) NOT NULL
+);
