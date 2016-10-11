45080181374b630b3af55dab2f11326ca6677a37
reillyethan <lekskazimirchuk@gmail.com>
2016-10-11 10:52:06 +0300

    Uncommented mongo installation

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ apt-get install -y libpq-dev postgresql=$POSTGRES_VERSION\*
 sed -i 's@#listen_addresses@listen_addresses@' /etc/postgresql/$POSTGRES_VERSION/main/postgresql.conf
 
 sh -c "echo -e 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8' > /etc/default/locale"
-#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-#echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-#apt-get update
-#apt-get install -y mongodb-org
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+apt-get update
+apt-get install -y mongodb-org
