414c94ff424649ac50225910394c9027e1699bd5
ChernyakAleksei <lexai@inbox.ru>
2016-10-11 15:05:46 +0300

    Aleksei№5

---
+++ dumps/students/s_05/schema.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS customers CASCADE;
+DROP TABLE IF EXISTS personals CASCADE;
+DROP TABLE IF EXISTS providers CASCADE;
+DROP TABLE IF EXISTS roles CASCADE;
+DROP TABLE IF EXISTS close_seasons CASCADE;
+
+
+CREATE TABLE products (
+    id               SERIAL PRIMARY KEY,
+    name             VARCHAR(50) NOT NULL,
+    gender           VARCHAR(10) NOT NULL,
+    count            SMALLINT NOT NULL,
+    date_arrive      SMALLINT NOT NULL CONSTRAINT positive_date CHECK (date_arrive > 0)
+);
+
+CREATE TABLE customers (
+   id               SERIAL PRIMARY KEY,
+   public_name      VARCHAR(50) NOT NULL,
+   email            VARCHAR(20) NOT NULL,
+   discount         BOOLEAN,
+   discount_level   SMALLINT NOT NULL CONSTRAINT level_count CHECK (menace_level >= 5 AND menace_level <= 20),
+);
+
+CREATE TABLE personals (
+    id               SERIAL PRIMARY KEY,
+    person_id        SMALLINT NOT NULL,
+    name             VARCHAR(50) NOT NULL,
+    worked_hours     SMALLINT NOT NULL,
+);
+CREATE TABLE roles (
+  id               SERIAL PRIMARY KEY,
+  name             VARCHAR(50) NOT NULL,
+  person_id        INT REFERENCES personals (person_id)
+);
+
+CREATE TABLE providers (
+   id               SERIAL PRIMARY KEY,
+   name             VARCHAR(50) NOT NULL,
+   code             VARCHAR(10) NOT NULL,
+   country          VARCHAR(20) NOT NULL,
+);
+
+CREATE TABLE close_seasons(
+    id               SERIAL PRIMARY KEY,
+    season_name      VARCHAR(10) NOT NULL,
+    product_name     VARCHAR(50) NOT NULL REFERENCES products(name)
+);
